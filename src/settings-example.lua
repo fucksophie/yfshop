05e7d1cca541ef45bc1106dea9983338f8ec80f7
yourfriendoss <81387799+yourfriendoss@users.noreply.github.com>
2023-07-18 00:38:03 +0300

    provide an example shop

---
+++ src/settings-example.lua
@@ -0,0 +1,31 @@
+return {
+  ["address"] = "KHIHELLOWLRD",
+  ["privateKey"] = "MyPrivateKey",
+  ["topbar"] = {
+    ["managedBy"] = "exampleUser",
+    ["name"]      = "my shop",
+    ["smallText"] = false,
+  },
+  ["categories"] = {
+    ["ores"] = {
+      ["color"] = colors.red,
+      ["items"] = {
+        ["cblstn"] = {
+          ["name"] = "cobblestone",
+          ["id"] = "minecraft:cobblestone",
+          ["price"] = "0.015"
+        }
+      }
+    },
+    ["dyes"] = {
+      ["color"] = colors.green,
+      ["items"] = {
+        ["pdye"] = {
+          ["name"] = "pink dye",
+          ["id"] = "minecraft:pink_dye",
+          ["price"] = "1"
+        }
+      }
+    }
+  }
+}
