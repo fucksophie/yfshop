8621263cb057262ab1f0d434c2b92dc4a4477dda
yourfriendoss <81387799+yourfriendoss@users.noreply.github.com>
2023-07-25 02:07:52 +0300

    quickfix

--- src/krist.lua
+++ src/krist.lua
@@ -34,51 +34,67 @@ function krist.eventListener()
   while true do
     local event, transactionEvent = os.pullEvent("transaction")
     if transactionEvent.transaction.from ~= krist.krypton:makev2address(krist.settings.privateKey) then
-      local item = ""
-      local ret = ""
 
-      if transactionEvent.transaction.metadata then
-        if string.find(transactionEvent.transaction.metadata, "return") then
-          local splitted = split(transactionEvent.transaction.metadata, ";")
-          for _,v in pairs(splitted) do
-            if not string.find(v, "=") then
-              item = v
-            else
-              if string.find(v, "return=") then
-                ret = string.gsub(v, "return=", "")
+
+      local beingSentToMe = true
+      
+      if string.find(krist.settings.address, ".kst") then
+        if transactionEvent.transaction.sent_name then
+          if transactionEvent.transaction.sent_name ~= split(krist.settings.address, ".kst")[1] then
+            beingSentToMe = false
+          end
+        else
+          beingSentToMe = false
+        end
+      end
+
+      if beingSentToMe then
+        local item = ""
+        local ret = ""
+
+        if transactionEvent.transaction.metadata then
+          if string.find(transactionEvent.transaction.metadata, "return") then
+            local splitted = split(transactionEvent.transaction.metadata, ";")
+            for _,v in pairs(splitted) do
+              if not string.find(v, "=") then
+                item = v
+              else
+                if string.find(v, "return=") then
+                  ret = string.gsub(v, "return=", "")
+                end
+              end
+            end
+          else
+            item = transactionEvent.transaction.metadata
+            ret = transactionEvent.transaction.from
+          end
+
+          local whitelisted = false
+
+          if transactionEvent.transaction.sent_metaname then
+            item = transactionEvent.transaction.sent_metaname
+
+            if krist.settings.mnameWhitelist then
+              if has_value(krist.settings.mnameWhitelist, item) then
+                whitelisted = true
               end
             end
           end
-        else
-          item = transactionEvent.transaction.metadata
-          ret = transactionEvent.transaction.from
-        end
 
-        local whitelisted = false
+          if not whitelisted then
+            local result = krist.stock.buy(item, transactionEvent.transaction.value)
 
-        if transactionEvent.transaction.sent_metaname then
-          item = transactionEvent.transaction.sent_metaname
-
-          if krist.settings.mnameWhitelist then
-            if has_value(krist.settings.mnameWhitelist, item) then
-              whitelisted = true
+            if type(result) == "number" then
+              krist.ws:makeTransaction(ret, result, "You spent too much! Overflowed "..result.."KST.")
+            elseif type(result) == "boolean" then
+              if result then
+                krist.ws:makeTransaction(ret, transactionEvent.transaction.value, "Return (of item "..item..") to address "..ret.."")
+              end -- if this is false we do nothing
             end
           end
+        else
+          krist.ws:makeTransaction(transactionEvent.transaction.from, transactionEvent.transaction.value, ret.." please include a m-name!")
         end
-
-        if not whitelisted then
-          local result = krist.stock.buy(item, transactionEvent.transaction.value)
-
-          if type(result) == "number" then
-            krist.ws:makeTransaction(ret, result, "You spent too much! Overflowed "..result.."KST.")
-          elseif type(result) == "boolean" then
-            if result then
-              krist.ws:makeTransaction(ret, transactionEvent.transaction.value, "Return (of item "..item..") to address "..ret.."")
-            end -- if this is false we do nothing
-          end
-        end
-      else
-        krist.ws:makeTransaction(transactionEvent.transaction.from, transactionEvent.transaction.value, ret.." please include a m-name!")
       end
     end
   end
